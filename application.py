821a11149f060c314ea1d15152219d4349e4aad6
liljoker123 <lilantonyfreefire@gmail.com>
2019-10-19 23:34:00 +0000

    Inicializando proyecto

---
+++ application.py
@@ -0,0 +1,31 @@
+#db_uri: postgres://ddhaqtrukcarut:f17b57426ec1dff756bda6043d800e84ed5336facf76d28272d556da8daf25c8@ec2-54-243-238-226.compute-1.amazonaws.com:5432/d96kmas6a0dtna
+
+import os
+import requests
+
+from flask import Flask, session, render_template, request, jsonify, redirect, url_for
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+app.config["DEBUG"] = True
+Session(app)
+
+# Set up database
+engine = create_engine("postgres://ddhaqtrukcarut:f17b57426ec1dff756bda6043d800e84ed5336facf76d28272d556da8daf25c8@ec2-54-243-238-226.compute-1.amazonaws.com:5432/d96kmas6a0dtna")
+db = scoped_session(sessionmaker(bind=engine))
+
+@app.route("/")
+def index():
+    ii = db.execute("select * from ejemplo")
+    return render_template("index.htm", ii = ii)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
